140f24caaed94dfefcf30d8bbc7b9c0fa710b6b5
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-07 02:59:54 +0000

    Wrapper done

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,8 @@
+class DiceController < ApplicationController
+
+  def dice_roll
+    render ({:template => "pages_templates/home"})
+
+  end
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+  get("/", :controller => "homepage", :action => "home")
 
-  # get "/your_first_screen" => "pages#first"
-  
+  get("/dice/2/6", :controller => "dice", :action => "dice_roll")
+
+  get("/dice/2/10", :controller => "dice", :action => "dice_roll")
+
+  get("/dice/1/20", :controller => "dice", :action => "dice_roll")
+
+  get("/dice/5/4", :controller => "dice", :action => "dice_roll")
 end
