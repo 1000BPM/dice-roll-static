a9691924655d2432ca8fe37447f85c3f9478e578
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-07 03:21:06 +0000

    Complete draft

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,7 +1,20 @@
 class DiceController < ApplicationController
 
   def dice_roll
-    render ({:template => "pages_templates/home"})
+
+      @rolls = []
+      @dice_user_input = params.fetch("number_dice").to_i
+      @sides_user_input = params.fetch("number_sides").to_i
+
+      @dice_user_input.times do
+        die = rand(1..@sides_user_input)
+
+        @rolls.push(die)
+  
+      end
+
+    
+  render ({:template => "pages_templates/outcome"})
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
 
   get("/", :controller => "homepage", :action => "home")
 
-  get("/dice/2/6", :controller => "dice", :action => "dice_roll")
+  get("/dice/2/6", :controller => "dice", :action => "dice_roll", :number_dice => "2", :number_sides => "6")
 
-  get("/dice/2/10", :controller => "dice", :action => "dice_roll")
+  get("/dice/2/10", :controller => "dice", :action => "dice_roll", :number_dice => "2", :number_sides => "10")
 
-  get("/dice/1/20", :controller => "dice", :action => "dice_roll")
+  get("/dice/1/20", :controller => "dice", :action => "dice_roll", :number_dice => "1", :number_sides => "20")
 
-  get("/dice/5/4", :controller => "dice", :action => "dice_roll")
+  get("/dice/5/4", :controller => "dice", :action => "dice_roll", :number_dice => "5", :number_sides => "4")
 end
